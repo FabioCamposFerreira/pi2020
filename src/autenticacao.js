db74339d5fc33a5af75afe2324f51fcc95225026
FabioCamposFerreira <63975718+FabioCamposFerreira@users.noreply.github.com>
2020-12-16 22:39:00 -0300

    Update autenticacao.js

--- src/autenticacao.js
+++ src/autenticacao.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: "AIzaSyCMfgPfeL69cZFXa9YquBsVKkBjWvO_pjY",
-  authDomain: "clienteservidoriptv-e4d18.firebaseapp.com",
-  databaseURL: "https://clienteservidoriptv-e4d18.firebaseio.com",
-  projectId: "clienteservidoriptv-e4d18",
-  storageBucket: "clienteservidoriptv-e4d18.appspot.com",
-  messagingSenderId: "338921869834",
-  appId: "1:338921869834:web:f052ba28c83be2805385f3",
-  measurementId: "G-JVTL6B9YMB"
+  apiKey: "COMPLETECOMSEUFIREBASE",
+  authDomain: "COMPLETECOMSEUFIREBASE",
+  databaseURL: "COMPLETECOMSEUFIREBASE",
+  projectId: "COMPLETECOMSEUFIREBASE",
+  storageBucket: "COMPLETECOMSEUFIREBASE",
+  messagingSenderId: "COMPLETECOMSEUFIREBASE",
+  appId: "COMPLETECOMSEUFIREBASE",
+  measurementId: "COMPLETECOMSEUFIREBASE"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
